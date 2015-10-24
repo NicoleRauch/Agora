0ae32b127e4326897533dddc0c0ec983dc1eab62
Nicole Rauch <nicole.m@gmx.de>
2015-10-24 22:48:31 +0200

    Turned ManagementComponent into a proper class because it needs to fetch data from the backend on mount.

--- socrates/frontend/javascript/management/management.js
+++ socrates/frontend/javascript/management/management.js
@@ -1,24 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import Addresses from './addresses.js';
 
-const ManagementComponent = () => (
-  <div>
-    Management for the win!
-    <Addresses displayed='Teilnehmer' data={[{
-    firstname: 'A1', lastname: 'AA', email: 'a@a', homeAddressLines: 'AH', billingAddressLines: 'AB', resourceNames: 'AR'
-    },
-    {
-    firstname: 'A2', lastname: 'AA', email: 'a@a', homeAddressLines: 'AH', billingAddressLines: 'AB', resourceNames: 'AR'
-    },
-    {
-    firstname: 'A3', lastname: 'AA', email: 'a@a', homeAddressLines: 'AH', billingAddressLines: 'AB', resourceNames: 'AR'
-    }]} />
-    <hr />
-    <Addresses displayed='Wartenden' data={[{
+import Addresses from './addresses.js';
+import * as actions from './Actions.js';
+
+export class ManagementComponent extends Component {
+
+  componentDidMount() {
+    this.props.dispatch(actions.loadAddonLines());
+  }
+
+  render() {
+    return (
+      <div>
+        Management for the win!
+        <Addresses displayed='Teilnehmer' data={this.props.addonLines}/>
+        <hr />
+        <Addresses displayed='Wartenden' data={[{
     firstname: 'B', lastname: 'BB', email: 'b@b', homeAddressLines: 'BH', billingAddressLines: 'BaB', resourceNames: 'BR'
-    }]} />
-  </div>
-);
+    }]}/>
+      </div>
+    );
+  }
+}
 
 export default connect(state => state)(ManagementComponent);
