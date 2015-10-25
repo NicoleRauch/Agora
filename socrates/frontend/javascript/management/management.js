e63afbb1b49e62b1da0a2d3a4c2147eb6cf832b4
Nicole Rauch <nicole.m@gmx.de>
2015-10-25 15:46:17 +0100

    Introducing tabs on the management page.

--- socrates/frontend/javascript/management/management.js
+++ socrates/frontend/javascript/management/management.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 import Addresses from './addresses.js';
 import * as actions from './Actions.js';
@@ -12,13 +13,43 @@ export class ManagementComponent extends Component {
 
   render() {
     return (
-      <div>
-        Management for the win!
-        <Addresses displayed='Teilnehmer' data={this.props.addonLines}/>
-        <hr />
-        <Addresses displayed='Wartenden' data={[{
+      <div className='row'>
+        <div className='col-md-12'>
+          <div className='page-header'>
+            <h2>
+              SoCraTes 2015 - Verwaltung
+              <a href='/registration' className='btn pull-right btn-default'>Zurück</a>
+            </h2>
+          </div>
+          <Tabs selectedIndex={0}>
+            <TabList>
+              <Tab>Übersicht</Tab>
+              <Tab>Participants</Tab>
+              <Tab>Wartelisten</Tab>
+              <Tab>Zahlungen</Tab>
+              <Tab>Zimmerzuordnungen</Tab>
+              <Tab>T-Shirt-Bestellungen</Tab>
+              <Tab>Zusatzinformationen</Tab>
+              <Tab>Abgemeldete</Tab>
+              <Tab>Adressen</Tab>
+            </TabList>
+            <TabPanel>A</TabPanel>
+            <TabPanel>B</TabPanel>
+            <TabPanel>C</TabPanel>
+            <TabPanel>D</TabPanel>
+            <TabPanel>E</TabPanel>
+            <TabPanel>F</TabPanel>
+            <TabPanel>G</TabPanel>
+            <TabPanel>H</TabPanel>
+            <TabPanel>
+              <Addresses displayed='Teilnehmer' data={this.props.addonLines}/>
+              <hr />
+              <Addresses displayed='Wartenden' data={[{
     firstname: 'B', lastname: 'BB', email: 'b@b', homeAddress: 'BH', billingAddress: 'BaB', resourceNames: 'BR'
     }]}/>
+            </TabPanel>
+          </Tabs>
+        </div>
       </div>
     );
   }
